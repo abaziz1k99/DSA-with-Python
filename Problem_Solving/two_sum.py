c289428f9933250e3105a20ca903f4781c0069ba
abaziz1k99 <abdulaziz1k99@outlook.com>
2026-01-06 22:08:52 +0600

    solve with hashmap

--- Problem_Solving/two_sum.py
+++ Problem_Solving/two_sum.py
@@ -7,12 +7,23 @@ target = 9
 
 def two_sum_BF(nums):
     for i, num in enumerate(nums):
-        remaing = target - num
+        remaining = target - num
         for x in range(i + 1, len(nums)):
-            if nums[x] == remaing:
+            if nums[x] == remaining:
                 flag = True
                 return (i, x)
-        return ()
+    return ()
 
 
+def two_sum_hash(nums: list) -> tuple:
+    seen = {}
+
+    for i, num in enumerate(nums):
+        remaining = target - num
+        if remaining in seen:
+            return (seen[remaining], i)
+        seen[num] = i
+
+
+print(two_sum_hash(values))
 print(two_sum_BF(values))
